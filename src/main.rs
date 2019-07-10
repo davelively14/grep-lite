a6208d40b789b1674fd3709ad0e1e66f58697d38
davelively14 <dave.lively@salesloft.com>
2019-07-09 20:45:32 -0400

    Added arrays

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
+    basic_grep();
+    arrays();
+}
+
+fn basic_grep() {
     let search_term = "picture";
     let quote = "Every face, every shop, bedroom window, public-house and
 dark square is a picture feverishly turned--in search of what?
@@ -9,5 +14,29 @@ It is the same with books. What do we seek through millions of pages?";
             let line_num = idx + 1;
             println!("{}: {}", line_num, line);
         }
+    }
+}
+
+fn arrays() {
+    let one = [1, 2, 3];
+    let two: [u8; 3] = [1, 2, 3];
+    let blank1 = [0; 3];
+    let blank2: [u8; 3] = [0; 3];
+
+    let arrays = [one, two, blank1, blank2];
+
+    for a in &arrays {
+        print!("{:?}: ", a);
+        for n in a.iter() {
+            print!("\t{} + 10 = {}", n, n + 10);
+        }
+
+        let mut sum = 0;
+        for i in 0..a.len() {
+            sum += a[i];
+        }
+
+        print!("\t(Σ{:?} = {})", a, sum);
+        println!("");
     }
 }
